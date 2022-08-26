55c87004d65ad88920b06a44f69059b0767f3b12
Aishen <acaberdin@gmail.com>
2022-08-27 00:23:19 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.lenth) {
+    currentActive = circles.lenth;
+  }
+
+  update();
+});
+
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+
+  progress.style.width = ((currentActive - 1) / (circles.length - 1)) * 100 + '%';
+
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
+}
